ba797f01051c56905b834815337f6518dadd105d
Alejandro Granados <alegranados03@gmail.com>
2022-10-27 06:48:56 -0600

    server side render character page, mock structure

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -1,17 +1,30 @@
-import { ROUTES } from "../../routes/routes";
+import { API_ROUTES, ROUTES } from "../../routes/routes";
+import { ICharacter } from "../../types/character.interface";
 
-export async function getStaticProps() {
-    const response = await fetch(ROUTES.static.characters);
-    const characters = await response.json();
-    return {
-      props: {
-        characters,
-      },
-    };
-  }
-  
-const CharactersProfilePage = (): JSX.Element => {
-  return <>TAREA</>;
+
+export async function getServerSideProps(context: any) {
+  const id = context.params.id;
+  const response = await fetch(API_ROUTES.dynamics.character(id));
+  const character = await response.json();
+
+  return { props: { character } };
+}
+
+interface CharacterProfileProps {
+  character: ICharacter;
+}
+
+const CharactersProfilePage = ({
+  character,
+}: CharacterProfileProps): JSX.Element => {
+  return (
+    <div>
+      <p>{character.name}</p>
+      <p>{character.gender}</p>
+      <p>{character.status}</p>
+      <p>{character.location.name}</p>
+    </div>
+  );
 };
 
 export default CharactersProfilePage;
