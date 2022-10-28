19db2e725eead74112b6a6cf6501f06d0a94238a
Alejandro Granados <alegranados03@gmail.com>
2022-10-28 16:30:30 -0600

    episodes route added, episode interface created

--- routes/routes.ts
+++ routes/routes.ts
@@ -3,9 +3,17 @@ export const API_ROUTES = {
   static: { characters: `${API_URL}/character` },
   dynamics: {
     character: (characterId: number) => `${API_URL}/character/${characterId}`,
+    episodes: (episodes: number | number[]) => {
+      const url = `${API_URL}/episode/`;
+      if (Array.isArray(episodes)) {
+        return `${url}${episodes.join(",")}`;
+      }
+      return `${url}${episodes}`;
+    },
   },
 };
 
 export const ROUTES = {
+  index: `/`,
   character: (characterId: number) => `/characters/${characterId}`,
 };
